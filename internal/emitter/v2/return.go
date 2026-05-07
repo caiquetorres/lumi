c339ebc15ea2b2c478c66538de75e8fbd289bed7
Caique Torres <caiquet.bastos@gmail.com>
2026-05-07 15:12:14 -0300

    feat: add handling for return statements with nil expressions in emitter

--- internal/emitter/v2/return.go
+++ internal/emitter/v2/return.go
@@ -5,6 +5,9 @@ import "github.com/caiquetorres/lumi/internal/parser"
 func (e *Emitter) emitReturn(re *parser.ReturnStmt) {
 	if re.Expr != nil {
 		e.emitExpr(re.Expr)
+	} else {
+		e.ch.emit(PushInt)
+		e.ch.emitUint32(0)
 	}
 
 	e.ch.emit(Return)
